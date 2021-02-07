effb6826b1f120860a55c1d603ffda868fc82df4
mickguel <mickguel@gmail.com>
2021-02-07 09:04:40 +0100

    Corregidos errores y sidebar collapsable

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 (function($){"use strict";
 	$('#sidebarCollapse').on('click',function(){
-		// $('#sidebar, #content').toggleClass('active');
-		$('#sidebar').toggleClass('d-none');
-		// $('#button-collapse').toggleClass('fa-bars fa-times');
+		$('#sidebar_menu').toggleClass('d-none');
 	});
-	// Sidebar - Collapse-icon, hide submmenu
-	$('.collapse').collapse('hide');
-	$('#collapse-icon').addClass('fa-angle-double-left');
-	$('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
 })
 (jQuery);
